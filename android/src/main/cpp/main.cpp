acc1b9ce180336205eb7ce9fa82116beaae6b2af
markvideon <mark.videon@gmail.com>
2024-04-21 21:39:07 +0930

    Resolved dependency issues

--- android/src/main/cpp/main.cpp
+++ android/src/main/cpp/main.cpp
@@ -1,10 +1,10 @@
 #include "paddleboat/paddleboat.h"
 #include "jni.h"
 
-JNIEXPORT void JNICALL destroy(JNIEnv *env) {
+JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_destroy(JNIEnv *env, jobject jcontext) {
     return Paddleboat_destroy(env);
 }
 
-JNIEXPORT int32_t JNICALL init(JNIEnv *env, jobject jcontext) {
+JNIEXPORT jint JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_init(JNIEnv *env, jobject jcontext) {
     return Paddleboat_init(env, jcontext);
 }
