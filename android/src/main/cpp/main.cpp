d091ff34aae0682326e2c47bf341d42b2c4f033b
markvideon <mark.videon@gmail.com>
2024-04-21 12:02:24 +0930

    Inching closer... Various compiliation errors due to dependencies games-controller source has that it doesn't ship with.

--- android/src/main/cpp/main.cpp
+++ android/src/main/cpp/main.cpp
@@ -1,1 +1,10 @@
 #include "paddleboat/paddleboat.h"
+#include "jni.h"
+
+JNIEXPORT void JNICALL destroy(JNIEnv *env) {
+    return Paddleboat_destroy(env);
+}
+
+JNIEXPORT int32_t JNICALL init(JNIEnv *env, jobject jcontext) {
+    return Paddleboat_init(env, jcontext);
+}
--- android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
+++ android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
@@ -9,32 +9,76 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** GamepadsAndroidPlugin */
+// Get Context: FlutterPlugin.FlutterPluginBinding.getApplicationContext();
 class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "gamepads_android")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  companion object {
+    init {
+        java.lang.System.loadLibrary("c++_shared")
+        java.lang.System.loadLibrary("gamepads_android")
     }
   }
 
-  fun ayo() {
-    
+  external fun destroy();
+  external fun init(): Int;
+
+  /*
+  external fun addControllerRemapData(const Int remapTableEntryCount);
+  external fun addControllerRemapDataFromFd();
+  external fun addControllerRemapDataFromFileBuffer();
+  external fun getActiveAxisMask();
+  external fun getBackButtonConsumed();
+  external fun getControllerData();
+  external fun getControllerInfo();
+  external fun getControllerName();
+  external fun getControllerStatus();
+  external fun getIntegratedMotionSensorFlags();
+  external fun getLastKeycode();
+  external fun getMouseData();
+  external fun getMouseStatus();
+  external fun getPhysicalKeyboardStatus();
+  external fun isInitialized();
+  external fun onStart();
+  external fun onStop();
+  external fun processInputEvent();
+  external fun setBackButtonConsumed();
+  external fun setControllerLight();
+  external fun setControllerStatusCallback();
+  external fun setControllerVibrationData();
+  external fun setMotionDataCallback();
+  external fun setMotionDataCallbackWithIntegratedFlags();
+  external fun setMouseStatusCallback();
+  external fun setPhysicalKeyboardStatusCallback();
+  external fun update();
+  */
+
+  fun listGamepads(): List<Map<String, String>>  {
+    return mockListGamepads()
+  }
+
+  fun mockListGamepads() : List<Map<String, String>> {
+    return listOf(
+        mapOf("id" to "mockId", "name" to "mockName")
+    );
+  }
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "xyz.luan/gamepads")
+    channel.setMethodCallHandler(this)
+    init()
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-
+    destroy()
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "listGamepads") {
+      result.success(listGamepads())
+    } else {
+      result.notImplemented()
+    }
   }
 }
