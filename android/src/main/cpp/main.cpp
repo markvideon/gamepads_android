3b46fd974a91cf70f98182b7697b3847398ed845
markvideon <mark.videon@gmail.com>
2024-04-27 14:05:45 +0930

    Working init, destroy, isInitialised. Update is linked in principle, but untested

--- android/src/main/cpp/main.cpp
+++ android/src/main/cpp/main.cpp
@@ -1,40 +1,31 @@
-#include "paddleboat/paddleboat.h"
+#include "paddleboat.h"
 #include "jni.h"
 #include <android/log.h>
 
 extern "C" {
-    JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_init
-            (JNIEnv * env, jobject jcontext) {
+    JNIEXPORT jint JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_init
+            (JNIEnv * env, jobject plugin, jobject activity) {
         __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Initialising Paddleboat %d.%d.%d / %d", PADDLEBOAT_MAJOR_VERSION, PADDLEBOAT_MINOR_VERSION, PADDLEBOAT_BUGFIX_VERSION, PADDLEBOAT_PACKED_VERSION);
+        return Paddleboat_init(env, activity);
+    }
 
-        // init causes crash
-        // __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat init result: %d", Paddleboat_init(env, jcontext));
-
-        // destroy causes crash
-        // __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat before destroy");
-        // Paddleboat_destroy(env);
-        // __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat after destroy");
+    JNIEXPORT jboolean JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_isInitialised
+            (JNIEnv * env, jobject plugin) {
+        return Paddleboat_isInitialized();
     }
 
     JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_destroy
-    (JNIEnv * env, jobject jcontext) {
-        Paddleboat_destroy(env);
-        __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat destroyed");
-    }
-
-    JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_setControllerStatusCallback
-    (JNIEnv * env, jobject jcontext) {
-        // ToDo:
-    }
-
-    JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_setMotionDataCallback
-    (JNIEnv * env, jobject jcontext) {
-        // ToDo:
+    (JNIEnv * env, jobject plugin) {
+        if (Paddleboat_isInitialized()) {
+            Paddleboat_destroy(env);
+            __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat destroyed");
+        } else {
+            __android_log_print(ANDROID_LOG_INFO, "JNI_GamepadsAndroidPlugin", "Paddleboat was not destroyed, was not initialised.");
+        }
     }
 
     JNIEXPORT void JNICALL Java_com_example_gamepads_1android_GamepadsAndroidPlugin_update
-    (JNIEnv * env, jobject jcontext) {
+    (JNIEnv * env, jobject plugin) {
         Paddleboat_update(env);
     }
 }
-
--- android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
+++ android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
@@ -1,75 +1,46 @@
 package com.example.gamepads_android
 
 import androidx.annotation.NonNull
-import androidx.annotation.Keep
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import android.util.Log
+import android.app.Activity
 
-/** GamepadsAndroidPlugin */
-// Get Context: FlutterPlugin.FlutterPluginBinding.getApplicationContext();
-@Keep
-class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler {
+class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
-
+  private val TAG = "GamepadsAndroidPlugin"
   init {
-      Log.i("GamepadsAndroidPlugin", "before load library")
       System.loadLibrary("c++_shared")
       System.loadLibrary("gamepads_android")
-      Log.i("GamepadsAndroidPlugin", "after load library")
   }
 
   external fun destroy();
-  external fun init();
-
-  external fun setControllerStatusCallback();
-  external fun setMotionDataCallback();
+  external fun init(activity: Activity): Int;
+  external fun getControllerData(); // Unimplemented
+  external fun getControllerInfo(); // Unimplemented
+  external fun getControllerName(); // Unimplemented
+  external fun getControllerStatus(); // Unimplemented
+  external fun isInitialised(): Boolean;
+  external fun onStart(); // Unimplemented
+  external fun onStop(); // Unimplemented
   external fun update();
 
-  /*
-  external fun addControllerRemapData(const Int remapTableEntryCount);
-  external fun addControllerRemapDataFromFd();
-  external fun addControllerRemapDataFromFileBuffer();
-  external fun getActiveAxisMask();
-  external fun getBackButtonConsumed();
-  external fun getControllerData();
-  external fun getControllerInfo();
-  external fun getControllerName();
-  external fun getControllerStatus();
-  external fun getIntegratedMotionSensorFlags();
-  external fun getLastKeycode();
-  external fun getMouseData();
-  external fun getMouseStatus();
-  external fun getPhysicalKeyboardStatus();
-  external fun isInitialized();
-  external fun onStart();
-  external fun onStop();
-  external fun processInputEvent();
-  external fun setBackButtonConsumed();
-  external fun setControllerLight();
-  external fun setControllerVibrationData();
-  external fun setMotionDataCallbackWithIntegratedFlags();
-  external fun setMouseStatusCallback();
-  external fun setPhysicalKeyboardStatusCallback();
-  */
-
   fun listGamepads(): List<Map<String, String>>  {
-    Log.i("GamepadsAndroidPlugin", "listGamepads()")
     return mockListGamepads()
   }
 
   fun mockListGamepads() : List<Map<String, String>> {
-    Log.i("GamepadsAndroidPlugin", "mockListGamepads()")
     return listOf(
         mapOf("id" to "mockId", "name" to "mockName")
     );
   }
 
   fun mockGamepadEvent() : Map<String, Any> {
-    Log.i("GamepadsAndroidPlugin", "mockGamepadEvent()")
     return mapOf(
             "gamepadId" to "mockId",
             "time" to 0,
@@ -79,11 +50,24 @@ class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler {
     )
   }
 
+  override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
+    val paddleboatInitResult = init(activityPluginBinding.activity)
+    if (!isInitialised()) {
+      Log.i(TAG, "Paddleboat failed to initialise. Error code: ${paddleboatInitResult}")
+    }
+  }
+
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "xyz.luan/gamepads")
     channel.setMethodCallHandler(this)
+  }
 
-    init()
+  override fun onDetachedFromActivity() {
+    destroy()
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    destroy()
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
@@ -95,6 +79,13 @@ class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler {
       result.success(listGamepads())
     } else {
       result.notImplemented()
+    }
+  }
+
+  override fun onReattachedToActivityForConfigChanges(activityPluginBinding: ActivityPluginBinding) {
+    val paddleboatInitResult = init(activityPluginBinding.activity)
+    if (!isInitialised()) {
+      Log.i(TAG, "Paddleboat failed to initialise. Error code: ${paddleboatInitResult}")
     }
   }
 }
