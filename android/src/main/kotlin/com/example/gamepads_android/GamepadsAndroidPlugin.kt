928f56ee4aef2cbd35570779ecd2963ed20c53f5
markvideon <mark.videon@gmail.com>
2024-05-13 18:23:44 +0930

    Removed dependence on game-controller, we are not the intended audience

--- android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
+++ android/src/main/kotlin/com/example/gamepads_android/GamepadsAndroidPlugin.kt
@@ -1,6 +1,10 @@
 package com.example.gamepads_android
 
 import androidx.annotation.NonNull
+import android.app.Activity
+import android.util.Log
+import android.view.InputDevice
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -8,39 +12,27 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import android.util.Log
-import android.app.Activity
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
+
 
 class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
   private val TAG = "GamepadsAndroidPlugin"
-  init {
-      System.loadLibrary("c++_shared")
-      System.loadLibrary("gamepads_android")
-  }
 
-  external fun destroy();
-  external fun init(activity: Activity): Int;
-  external fun getControllerData(); // Unimplemented
-  external fun getControllerInfo(); // Unimplemented
-  external fun getControllerName(); // Unimplemented
-  external fun getControllerStatus(); // Unimplemented
-  external fun isInitialised(): Boolean;
-  external fun onStart(); // Unimplemented
-  external fun onStop(); // Unimplemented
-  external fun update();
+  private lateinit var thread: Thread
 
-  fun listGamepads(): List<Map<String, String>>  {
+  private fun listGamepads(): List<Map<String, String>>  {
     return mockListGamepads()
   }
 
-  fun mockListGamepads() : List<Map<String, String>> {
+  private fun mockListGamepads() : List<Map<String, String>> {
     return listOf(
         mapOf("id" to "mockId", "name" to "mockName")
     );
   }
 
-  fun mockGamepadEvent() : Map<String, Any> {
+  private fun mockGamepadEvent() : Map<String, Any> {
     return mapOf(
             "gamepadId" to "mockId",
             "time" to 0,
@@ -50,11 +42,33 @@ class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
     )
   }
 
-  override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
-    val paddleboatInitResult = init(activityPluginBinding.activity)
-    if (!isInitialised()) {
-      Log.i(TAG, "Paddleboat failed to initialise. Error code: ${paddleboatInitResult}")
+  // https://developer.android.com/develop/ui/views/touch-and-input/game-controllers/controller-input#input
+  fun getGameControllerIds(): List<Int> {
+    val gameControllerDeviceIds = mutableListOf<Int>()
+    val deviceIds = InputDevice.getDeviceIds()
+    deviceIds.forEach { deviceId ->
+      InputDevice.getDevice(deviceId).apply {
+        if (this != null) {
+          // Verify that the device has gamepad buttons, control sticks, or both.
+          if (sources and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD
+                  || sources and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK) {
+            // This device is a game controller. Store its device ID.
+            gameControllerDeviceIds
+                    .takeIf { !it.contains(deviceId) }
+                    ?.add(deviceId)
+          }
+        }
+      }
     }
+    return gameControllerDeviceIds
+  }
+
+  override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
+    onAttachedToActivityShared(activityPluginBinding.activity)
+  }
+
+  fun onAttachedToActivityShared(activity: Activity) {
+
   }
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -63,11 +77,15 @@ class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   }
 
   override fun onDetachedFromActivity() {
-    destroy()
+    onDetachedFromActivityShared()
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    destroy()
+    onDetachedFromActivityShared()
+  }
+
+  fun onDetachedFromActivityShared() {
+
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
@@ -83,9 +101,6 @@ class GamepadsAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   }
 
   override fun onReattachedToActivityForConfigChanges(activityPluginBinding: ActivityPluginBinding) {
-    val paddleboatInitResult = init(activityPluginBinding.activity)
-    if (!isInitialised()) {
-      Log.i(TAG, "Paddleboat failed to initialise. Error code: ${paddleboatInitResult}")
-    }
+    onAttachedToActivityShared(activityPluginBinding.activity)
   }
 }
